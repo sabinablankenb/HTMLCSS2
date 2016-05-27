45549371f0e1eaff93e784521ee9e09cfb065318
sabinablankenb <sabinablankenberg@gmail.com>
2016-05-27 10:19:00 -0700

    new

--- myjavascript.js
+++ myjavascript.js
@@ -13,3 +13,54 @@ function myFunctions() {
     }
     document.getElementById("demo").innerHTML = text;
 }
+function numbers(){
+    window.location = "numbers.html";
+
+}
+function Average(){
+    window.location = "mean.html";
+    nums.sort();
+    var total = 0;
+    var length = 0;
+
+    for(var i = 0; i < nums.length; i++){
+        total += nums[i];
+        console.log(nums);
+    }
+    length = total/nums.length
+    document.write(length);
+}
+
+function median() {
+    window.location = "median.html";
+    var half = Math.floor(nums.length/2);
+    //document.write(half);
+    if(nums.length % 2)
+    {
+        document.write(nums[half]);
+
+    }
+    else{
+      document.write((nums[half-1] + nums[half]) / 2.0);
+    }
+}
+function mode(){
+    window.location = "mode.html";
+}
+  var numMapping = {};
+    for(var i = 0; i < nums.length; i++){
+        if(numMapping[nums[i]] === undefined){
+            numMapping[nums[i]] = 0;
+        }
+            numMapping[nums[i]] += 1;
+    }
+    var greatestFreq = 0;
+    var mode;
+    for(var prop in numMapping){
+        if(numMapping[prop] > greatestFreq){
+            greatestFreq = numMapping[prop];
+            mode = prop;
+        }
+    }
+    document.write(parseInt(mode));
+
